ad27961e927f7dee24dc237be5478f9089ca637a
Ramiro Detke <ramiro.fer.det@gmail.com>
2022-10-18 17:09:33 -0300

    updated examples to use SimplePycomParser

--- examples/discover_devices.py
+++ examples/discover_devices.py
@@ -1,9 +1,9 @@
 from pycom import find_pycom_nodes
+from pycom import SimplePycomParser
 
 devices = find_pycom_nodes(timeout=3)
 
 for dev in devices:
     dev.initialize()
     dev.help()
-
-print('hola')
+    print(SimplePycomParser.get_value(device.get_temperature()))
--- examples/known_device.py
+++ examples/known_device.py
@@ -1,7 +1,10 @@
 from pycom import PycomNode
+from pycom import SimplePycomParser
 
 # Uncomment and fill with IP and PORT
 # device = PycomNode(ip=<IP>, port=<PORT>)
 
 device.initialize()     #initializes the device
 device.help()           #prints available methods
+
+print(SimplePycomParser.get_value(device.get_temperature()))
